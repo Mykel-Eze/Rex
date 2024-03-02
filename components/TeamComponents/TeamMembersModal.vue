175377852272b6166f36e1855d043010cd6c45f2
Mykel-Eze <michaeleze1997@gmail.com>
2024-03-02 05:30:28 +0100

    Team members added

--- components/TeamComponents/TeamMembersModal.vue
+++ components/TeamComponents/TeamMembersModal.vue
@@ -1,11 +1,11 @@
 <template>
     <!--=== Mr Eyitayo Shiyanbola (CEO/MD) ===-->
-  <div id="team-member-1" class="modal team-modal">
+  <div id="Eyitayo-Shiyanbola" class="modal team-modal">
     <img src="~/assets/images/close.svg" alt="close" class="modal-close">
     <div class="modal-content">
         <div class="member-modal-wrapper">
             <div class="member-img-side">
-                <img src="~/assets/images/team-member-1.jpg" alt="team-img" class="member-img">
+                <img src="~/assets/images/teams/Eyitayo-Shiyanbola.jpg" alt="Eyitayo Shiyanbola" class="member-img">
             </div>
 
             <div class="member-txt-side">
@@ -13,7 +13,8 @@
                     <div class="member-role">MD/CEO:</div>
                     <div class="member-name">Mr Eyitayo Shiyanbola.</div>
                     <div class="member-desc">
-                        Over 15 years’ experience in Real Estate practice/business in Nigeria.
+                        <p>BSc (Estate Management), MBA; ANIVS;</p>
+                        <p>Over 15 years' experience in real estate practice/business in Nigeria.</p>
                     </div>
                     <a href="http://linkedin.com" class="member-linkedin-wrapper flex-div gap-2" target="_blank" rel="noopener noreferrer">
                         <img src="~/assets/images/linkedin-black.svg" alt="linkedin" class="linkedin-icon">
@@ -25,12 +26,12 @@
     </div>
   </div>
 
-  <div id="team-member-2" class="modal team-modal">
+  <div id="Tobi-Shiyanbola" class="modal team-modal">
     <img src="~/assets/images/close.svg" alt="close" class="modal-close">
     <div class="modal-content">
         <div class="member-modal-wrapper">
             <div class="member-img-side">
-                <img src="~/assets/images/big-team-img-2.jpg" alt="team-img" class="member-img">
+                <img src="~/assets/images/teams/Tobi-Shiyanbola.jpg" alt="Tobi Shiyanbola" class="member-img">
             </div>
 
             <div class="member-txt-side">
@@ -38,8 +39,11 @@
                     <div class="member-role">Executive Director:</div>
                     <div class="member-name">Mrs Tobi Shiyanbola.</div>
                     <div class="member-desc">
-                        Tobi has some experience in media, research, and customer service. She's a client advocate at
-                        heart, and is constantly exploring ways to improve client experience and satisfaction
+                        <p>BSc (Psychology). MSc (Educational Psychology).</p>
+                        <p>
+                            Tobi has some experience in media, research, and customer service. She's a client advocate
+                            at heart, and is constantly exploring ways to improve client experience and satisfaction.
+                        </p>
                     </div>
                     <a href="http://linkedin.com" class="member-linkedin-wrapper flex-div gap-2" target="_blank" rel="noopener noreferrer">
                         <img src="~/assets/images/linkedin-black.svg" alt="linkedin" class="linkedin-icon">
@@ -51,12 +55,12 @@
     </div>
   </div>
 
-  <div id="team-member-3" class="modal team-modal">
+  <div id="Adeola-Omogbehinmi" class="modal team-modal">
     <img src="~/assets/images/close.svg" alt="close" class="modal-close">
     <div class="modal-content">
         <div class="member-modal-wrapper">
             <div class="member-img-side">
-                <img src="~/assets/images/team-member-3.jpg" alt="team-img" class="member-img">
+                <img src="~/assets/images/teams/Adeola-Omogbehinmi.jpg" alt="Adeola Omogbehinmi" class="member-img">
             </div>
 
             <div class="member-txt-side">
@@ -64,7 +68,12 @@
                     <div class="member-role">Operations Manager:</div>
                     <div class="member-name">Mrs Adeola Omogbehinmi.</div>
                     <div class="member-desc">
-                        Over 10 years of experience in real estate practice and operations in Nigeria.
+                        <p>
+                            OND (Hospitality management), Bpharm(pharmacy).
+                        </p>
+                        <p>
+                            Over 10 years of experience in real estate practice and operations in Nigeria.
+                        </p>
                     </div>
                     <a href="http://linkedin.com" class="member-linkedin-wrapper flex-div gap-2" target="_blank" rel="noopener noreferrer">
                         <img src="~/assets/images/linkedin-black.svg" alt="linkedin" class="linkedin-icon">
@@ -76,12 +85,12 @@
     </div>
   </div>
 
-  <div id="team-member-4" class="modal team-modal">
+  <div id="Tobiloba-Shiyanbola" class="modal team-modal">
     <img src="~/assets/images/close.svg" alt="close" class="modal-close">
     <div class="modal-content">
         <div class="member-modal-wrapper">
             <div class="member-img-side">
-                <img src="~/assets/images/team-member-4.jpg" alt="team-img" class="member-img">
+                <img src="~/assets/images/teams/team-member-4.jpg" alt="Tobiloba Shiyanbola" class="member-img">
             </div>
 
             <div class="member-txt-side">
@@ -91,6 +100,91 @@
                     <div class="member-desc">
                         With a track record of exceptional brand management skills, he has advanced my career through 
                         various opportunities, showcasing a penchant for excellence, innovation, and strategic planning.
+                    </div>
+                    <a href="http://linkedin.com" class="member-linkedin-wrapper flex-div gap-2" target="_blank" rel="noopener noreferrer">
+                        <img src="~/assets/images/linkedin-black.svg" alt="linkedin" class="linkedin-icon">
+                        <span>LinkedIn</span>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+  </div>
+
+  <div id="Agomoh-Victoria" class="modal team-modal">
+    <img src="~/assets/images/close.svg" alt="close" class="modal-close">
+    <div class="modal-content">
+        <div class="member-modal-wrapper">
+            <div class="member-img-side">
+                <img src="~/assets/images/teams/Agomoh-Victoria.jpg" alt="Agomoh Ogechi Victoria" class="member-img">
+            </div>
+
+            <div class="member-txt-side">
+                <div class="container">
+                    <div class="member-role">Customer Service Executive:</div>
+                    <div class="member-name">Agomoh Ogechi Victoria.</div>
+                    <div class="member-desc">
+                        Dedicated and result oriented customer care executive with years of experience in delivering exceptional
+                        customer service and optimizing call center efficiency. Proven track record of excellence in
+                        communication, call resolution customer satisfaction, and streamline operations. A graduate of Olabisi
+                        Onabanjo University, committed to maintaining the highest standards of service excellence in my organization.
+                    </div>
+                    <a href="http://linkedin.com" class="member-linkedin-wrapper flex-div gap-2" target="_blank" rel="noopener noreferrer">
+                        <img src="~/assets/images/linkedin-black.svg" alt="linkedin" class="linkedin-icon">
+                        <span>LinkedIn</span>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+  </div>
+
+  <div id="Ifelaja-Isaac" class="modal team-modal">
+    <img src="~/assets/images/close.svg" alt="close" class="modal-close">
+    <div class="modal-content">
+        <div class="member-modal-wrapper">
+            <div class="member-img-side">
+                <img src="~/assets/images/teams/Ifelaja-Isaac.jpg" alt="Ifelaja Isaac" class="member-img">
+            </div>
+
+            <div class="member-txt-side">
+                <div class="container">
+                    <div class="member-role">Company Accountant:</div>
+                    <div class="member-name">Ifelaja Isaac. O.</div>
+                    <div class="member-desc">
+                        Ifelaja Isaac. O. is a seasoned accounting and finance professional with a robust background in
+                        Accounting Applications, Tax Management, & All General Ledger Reports. He is known for his Accounting
+                        Software Proficiency. Outside the office, he finds fulfillment in teaching and mentoring young
+                        accounting, taxation & finance professionals.
+                    </div>
+                    <a href="http://linkedin.com" class="member-linkedin-wrapper flex-div gap-2" target="_blank" rel="noopener noreferrer">
+                        <img src="~/assets/images/linkedin-black.svg" alt="linkedin" class="linkedin-icon">
+                        <span>LinkedIn</span>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+  </div>
+
+  <div id="Kenneth-Nwimo" class="modal team-modal">
+    <img src="~/assets/images/close.svg" alt="close" class="modal-close">
+    <div class="modal-content">
+        <div class="member-modal-wrapper">
+            <div class="member-img-side">
+                <img src="~/assets/images/teams/Kenneth-Nwimo.jpg" alt="Kenneth Nwimo" class="member-img">
+            </div>
+
+            <div class="member-txt-side">
+                <div class="container">
+                    <div class="member-role">Company Lawyer:</div>
+                    <div class="member-name">Kenneth Nwimo.</div>
+                    <div class="member-desc">
+                        Kenneth Nwimo is a legal practitioner with expertise in legal compliance, property law, business
+                        development, data privacy, fintech, and start-up advisory. He holds an  LLB and B.L from the
+                        University of Benin and Lagos Law School, along with a  Financial Modeling and Valuation Analyst
+                        (FMVA) Certification with a distinction from the Corporate Finance Institute and a Diploma in
+                        Business Administration from  Oxford coupled with a host of other professional certifications.
                     </div>
                     <a href="http://linkedin.com" class="member-linkedin-wrapper flex-div gap-2" target="_blank" rel="noopener noreferrer">
                         <img src="~/assets/images/linkedin-black.svg" alt="linkedin" class="linkedin-icon">
--- components/TeamComponents/TeamMembersSection.vue
+++ components/TeamComponents/TeamMembersSection.vue
@@ -2,36 +2,61 @@
   <section id="team-members">
     <div class="row">
         <div class="team-members-wrapper">
-            <div class="team-members-block modal-trigger" data-target="team-member-1" data-aos="fade-up">
-                <img src="~/assets/images/team-member-1.jpg" alt="team-member" class="team-member-picture">
+            <div class="team-members-block modal-trigger" data-target="Eyitayo-Shiyanbola" data-aos="fade-up">
+                <img src="~/assets/images/teams/Eyitayo-Shiyanbola.jpg" alt="Eyitayo Shiyanbola" class="team-member-picture">
 
                 <div class="team-member-details">
                     <div class="team-member-name">Mr Eyitayo Shiyanbola</div>
                     <div class="team-member-role">MD/CEO</div>
                 </div>
             </div>
-            <div class="team-members-block modal-trigger" data-target="team-member-2" data-aos="fade-up">
-                <img src="~/assets/images/team-member-2.jpg" alt="team-member" class="team-member-picture">
+            <div class="team-members-block modal-trigger" data-target="Tobi-Shiyanbola" data-aos="fade-up">
+                <img src="~/assets/images/teams/Tobi-Shiyanbola.jpg" alt="Tobi Shiyanbola" class="team-member-picture">
 
                 <div class="team-member-details">
-                    <div class="team-member-name">Mrs Tobi Shiyanbola.</div>
+                    <div class="team-member-name">Mrs Tobi Shiyanbola</div>
                     <div class="team-member-role">Executive Director</div>
                 </div>
             </div>
-            <div class="team-members-block modal-trigger" data-target="team-member-3" data-aos="fade-up">
-                <img src="~/assets/images/team-member-3.jpg" alt="team-member" class="team-member-picture">
+            <div class="team-members-block modal-trigger" data-target="Adeola-Omogbehinmi" data-aos="fade-up">
+                <img src="~/assets/images/teams/Adeola-Omogbehinmi.jpg" alt="Adeola Omogbehinmi" class="team-member-picture">
 
                 <div class="team-member-details">
                     <div class="team-member-name">Mrs Adeola Omogbehinmi</div>
                     <div class="team-member-role">Operations Manager</div>
                 </div>
             </div>
-            <div class="team-members-block modal-trigger" data-target="team-member-4" data-aos="fade-up">
-                <img src="~/assets/images/team-member-4.jpg" alt="team-member" class="team-member-picture">
+            <div class="team-members-block modal-trigger" data-target="Tobiloba-Shiyanbola" data-aos="fade-up">
+                <img src="~/assets/images/teams/team-member-4.jpg" alt="Tobiloba Shiyanbola" class="team-member-picture">
 
                 <div class="team-member-details">
                     <div class="team-member-name">Mr. Tobiloba Shiyanbola</div>
                     <div class="team-member-role">Brand and Communications Manager</div>
+                </div>
+            </div>
+
+            <div class="team-members-block modal-trigger" data-target="Agomoh-Victoria" data-aos="fade-up">
+                <img src="~/assets/images/teams/Agomoh-Victoria.jpg" alt="Agomoh Ogechi Victoria" class="team-member-picture">
+
+                <div class="team-member-details">
+                    <div class="team-member-name">Agomoh Ogechi Victoria</div>
+                    <div class="team-member-role">Customer Service Executive</div>
+                </div>
+            </div>
+            <div class="team-members-block modal-trigger" data-target="Ifelaja-Isaac" data-aos="fade-up">
+                <img src="~/assets/images/teams/Ifelaja-Isaac.jpg" alt="Ifelaja Isaac" class="team-member-picture">
+
+                <div class="team-member-details">
+                    <div class="team-member-name">Ifelaja Isaac. O</div>
+                    <div class="team-member-role">Company Accountant</div>
+                </div>
+            </div>
+            <div class="team-members-block modal-trigger" data-target="Kenneth-Nwimo" data-aos="fade-up">
+                <img src="~/assets/images/teams/Kenneth-Nwimo.jpg" alt="Kenneth Nwimo" class="team-member-picture">
+
+                <div class="team-member-details">
+                    <div class="team-member-name">Kenneth Nwimo</div>
+                    <div class="team-member-role">Company Lawyer</div>
                 </div>
             </div>
         </div>
